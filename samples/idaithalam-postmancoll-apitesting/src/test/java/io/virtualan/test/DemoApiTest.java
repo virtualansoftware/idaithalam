d23f1bb31e02faae038454e155ec84c1f08794c8
Elan Thangamani <elans3.java@gmail.com>
2021-02-22 20:24:26 -0600

    Updated with Excel support

--- samples/idaithalam-postmancoll-apitesting/src/test/java/io/virtualan/test/DemoApiTest.java
+++ samples/idaithalam-postmancoll-apitesting/src/test/java/io/virtualan/test/DemoApiTest.java
@@ -1,29 +1,16 @@
 package io.virtualan.test;
 
 import io.virtualan.idaithalam.contract.IdaithalamExecutor;
-import io.virtualan.idaithalam.core.contract.validator.ExcelToCollectionGenerator;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Assert;
-import org.openapitools.OpenAPI2SpringBoot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import io.virtualan.idaithalam.core.UnableToProcessException;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = OpenAPI2SpringBoot.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 
 public class DemoApiTest {
     @Test
     public void validateContract()  {
         int status =0 ;
         try {
-            List<String> list = new ArrayList<>();
-            list.add("PetPost");
-            //ExcelToCollectionGenerator.createCollection(list, "virtualan_collection_pet.xlsx", "D:\\Elan\\virtualan-software-ws\\idaithalam\\excel-idaithalam\\idaithalam\\samples\\idaithalam-apitesting");
-            status = IdaithalamExecutor.validateContract("Pet API Production Checkout");//, "D:\\Elan\\virtualan-software-ws\\idaithalam\\excel-idaithalam\\idaithalam\\samples\\idaithalam-apitesting");
+            status = IdaithalamExecutor.validateContract("Pet API Production Checkout");
             System.out.println(status);
             if(status != 0) {
                 Assert.assertTrue(false);
