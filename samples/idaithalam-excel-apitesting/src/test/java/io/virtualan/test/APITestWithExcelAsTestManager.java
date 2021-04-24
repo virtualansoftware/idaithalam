65b75dbee9dd8ef792e81960d090fdf455f178d5
Elan Thangamani <elans3.java@gmail.com>
2021-04-23 20:50:18 -0500

    Add useful information for the error message

--- samples/idaithalam-excel-apitesting/src/test/java/io/virtualan/test/APITestWithExcelAsTestManager.java
+++ samples/idaithalam-excel-apitesting/src/test/java/io/virtualan/test/APITestWithExcelAsTestManager.java
@@ -11,7 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-public class APITestWithExcelAsRunManager {
+public class APITestWithExcelAsTestManager {
 
     @BeforeClass
     public void testBeforeClass(){
